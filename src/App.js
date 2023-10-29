d7464905d5451d64c8e053427a8e8694068bc277
faizan1699 <123071794+faizan1699@users.noreply.github.com>
2023-10-29 10:44:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,7 +67,8 @@ function App() {
         <>
            <Navbar navbrand="Faizan Rasheed" mode={mode} togglemode={togglemode} />
           <Routes>
-             <Route path="*" element={<Home />} />
+             <Route path="/" element={<Home />} />
+             <Route path="/home" element={<Home />} />
             <Route path="/About" element={<About />} />
             <Route path="/Services" element={<Services />} />
             <Route path="/Contact" element={<Contact sendMessage={sendMessage}/>}/>
